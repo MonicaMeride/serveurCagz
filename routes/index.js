af4e436c55ba7798dd80d263d0386acbaf6013f1
MonicaMeride <meride.monica@gmail.com>
2018-06-25 15:27:26 +0200

    db avec img

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
 var router = express.Router();
 let mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
@@ -17,6 +20,41 @@ let db = (table) => {
   });
 }
 
+router.post('/api/form',function(req,res){
+  console.log(req.body);
+  nodemailer.createTestAccount((err, account)=>{
+      const htmlEmail = "<h3>ContactDetails </h3><ul><li>Name: "+ req.body.nom +"</li><li>Email: "+req.body.email+"</li><li>Message: "+req.body.message+"</li></ul>";
+
+      let transporter = nodemailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 587,
+          auth: {
+            user: 'meride.monica@gmail.com',
+            pass: 'seremes971'
+          }
+      });
+
+      let mailOptions = {
+        from: req.body.email, // sender address
+        to: 'neutrino.agency@gmail.com', // list of receivers
+        replyTo: req.body.email,
+        subject: 'Hello ✔', // Subject line
+        text: req.body.message, // plain text body
+        html: htmlEmail // html body
+      };
+
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Message sent: %s', info.message);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      });
+
+  });
+});
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
@@ -30,7 +68,7 @@ router.get('/data', function(req,res) {
       let dbo = db.db("cagz-land");
       dbo.collection('choix').find().toArray(function(err, result) {
         if (err) throw err;
-        console.log(result[0].param_1);
+        console.log(result[0].levres);
         choix = result[0];
         
         db.close();
